5a6edc1ef34133c215801ef60577c1ca24983c25
Rohini0518 <rohinipolina0518@gmail.com>
2024-11-18 17:35:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Depolyment   </h1>
+    {/* TODO: 1.Implement the Portfolio application */}
+      {/* TODO: 2.Push the code to github */}
+      {/* TODO: 3.Deploy into Netlify */}
+    </>
+  )
+}
+
+export default App
